1eda75e45e331a37775b00dd686b3dbc08964972
hylian-flute <hylian_flute@yahoo.co.jp>
2019-10-26 18:54:43 +0000

    TweetDeck上でツイートに絵文字を追加できるように

--- extension/content.js
+++ extension/content.js
@@ -1,12 +1,12 @@
 const array = [
-"🤔",
-"✋",
-"💪",
-"🌱",
-"⚡️",
-"💧",
-"🚨",
-"❤️"
+    "🤔",
+    "✋",
+    "💪",
+    "🌱",
+    "⚡️",
+    "💧",
+    "🚨",
+    "❤️"
 ]
 
 const div = document.createElement("div");
@@ -24,22 +24,30 @@ div.style.backgroundColor = "rgba(29,161,242,0.5)";
 div.style.borderRadius = "2.5em";
 document.body.appendChild(div);
 
-
 function reqListener () {
     console.log(this.responseText);
     const number = JSON.parse(this.responseText);
     const words = number.map(i => array[i]);
     console.log(words);
-    div.innerHTML = words;
-    document.querySelector(".public-DraftStyleDefault-block").firstChild.firstChild.innerHTML += "!";
+    div.innerHTML = "";
+    words.forEach(word => {
+        const span = document.createElement("span");
+        span.innerHTML = word;
+        span.onclick = () => {
+            const textarea = document.querySelector(".js-compose-text");
+            textarea.value += word;
+        };
+        div.appendChild(span);
+    });
 }
   
 document.onkeydown = function(event){    
-    if (event.key === "Enter"){
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://localhost:8000");
-    oReq.send();
+    if (event.key === "Process" && event.code === "Enter"){
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", reqListener);
+        oReq.open("GET", "http://localhost:8000");
+        //oReq.open("GET", "http://192.168.33.10:8000");
+        oReq.send();
     }
 }
 
