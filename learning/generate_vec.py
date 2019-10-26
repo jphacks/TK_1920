5a092d16449569c198b43183a8feac9e09fa3f1f
hylian-flute <hylian_flute@yahoo.co.jp>
2019-10-26 16:51:29 +0000

    各絵文字の典型的な単語かつword2vecの語彙に含まれているものを抽出

--- learning/generate_vec.py
+++ learning/generate_vec.py
@@ -1,7 +1,21 @@
-from gensim.models import KeyedVectors
+from gensim.models.word2vec import Word2Vec
+import json
 
-model_dir = './learning/entity_vector/entity_vector.model.bin'
-model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
-results = model.most_similar(u'[指原莉乃]')
-for result in results:
-    print(result)
+model_path = './learning/word2vec_model/word2vec.gensim.model'
+model = Word2Vec.load(model_path)
+with open('./learning/tweet_data/ranking.json', 'r') as f:
+  ranking_list = json.load(f)
+
+data_list = []
+for row in ranking_list:
+  if row[0] <= 0.5:
+    break
+  try:
+    model.similarity(row[1], 'こんにちは')
+    if max(row[2]) >= 4:
+      data_list.append([row[1], row[2].index(max(row[2]))]);
+  except KeyError as error:
+    pass
+
+with open('./learning/tweet_data/typical_words.json', 'w') as f:
+  json.dump(data_list, f, ensure_ascii=False)
