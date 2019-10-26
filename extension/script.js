51cc4babd709cc1e155c3993b62be68604c9ca15
Naoki Ito <naoki@MacBook-Pro-3.local>
2019-10-26 17:06:56 +0900

    途中まで

--- extension/script.js
+++ extension/script.js
@@ -1,7 +1,7 @@
 $(function sjfjfsl() {
     window.alert("あああぶりぶりぶり");
 
-    $('.wrapper').css("display", "none");
+    // $('.wrapper').css("display", "none");
 
     // function closePopUp() {
     //     // console.log('マウスオーバーしました！');
@@ -15,28 +15,31 @@ $(function sjfjfsl() {
     // $('.r-30o5oe').mouseover(openPopUp);
     // $('.r-30o5oe').mouseout(closePopUp);
     document.onkeydown = e => console.log(e);
-    
-    $(".public-DraftEditor-content").on('keydown', function(e) {
-    //     $("body").append('<div id="modal-bg"></div>');
+
+    $(".public-DraftEditor-content").onkeydown(function() {
+         //body内の最後に<div id="modal-bg"></div>を挿入
+        $("body").append('<div class="wrapper"></div>');
  
-    // //画面中央を計算する関数を実行
-    //     modalResize();
+    //画面中央を計算する関数を実行
+        // modalResize();
  
-    // //モーダルウィンドウを表示
-    //     $("#modal-bg,#modal-main").fadeIn("slow");
+    //モーダルウィンドウを表示
+        $("#modal-main").fadeIn("fast");
  
-    // //画面のどこかをクリックしたらモーダルを閉じる
-    //     $("#modal-bg,#modal-main").click(function(){
-    //       $("#modal-main,#modal-bg").fadeOut("slow",function(){
-    //      //挿入した<div id="modal-bg"></div>を削除
-    //           $('#modal-bg').remove() ;
-    //      });
+    //画面のどこかをクリックしたらモーダルを閉じる
+        $("#modal-main").click(function(){
+        $("#modal-main").fadeOut("fast"
+        //function(){
+         //挿入した<div id="modal-bg"></div>を削除
+        //       $('#modal-bg').remove() ;
+        //  }
+         );
  
-    //     });
+        });
  
-    // //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
-    //  $(window).resize(modalResize);
-    //   function modalResize(){
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+    // $(window).resize(modalResize);
+    //     function modalResize(){
  
     //         var w = $(window).width();
     //       var h = $(window).height();
@@ -49,8 +52,8 @@ $(function sjfjfsl() {
     //             "left": ((w - cw)/2) + "px",
     //             "top": ((h - ch)/2) + "px"
     //       });
-    //     }
-        console.log('===== keydown ====='); // 1番目に実行される
+    //    }
+        // console.log('===== keydown ====='); // 1番目に実行される
         // console.log(e);
         // console.log(e.keyCode);
     });
