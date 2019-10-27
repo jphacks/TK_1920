8c1c1160d39539c8b5992b25b230f0449edae2dc
Naoki Ito <naoki@MacBook-Pro-3.local>
2019-10-27 11:17:18 +0900

    一通り終わりました。

--- extension/content.js
+++ extension/content.js
@@ -12,17 +12,25 @@ const array = [
 const div = document.createElement("div");
 div.className = "hoge";
 div.style.position = "absolute";
-div.style.display = "flex";
+div.style.display = "none";
 div.style.alignItems = "center";
 div.style.justifyContent = "center";
 div.style.fontSize = "23px";
 div.style.top = "152px";
-div.style.left = "4.8%";
+div.style.left = "70px";
 div.style.width = "250px";
 div.style.height = "50px";
 div.style.backgroundColor = "rgba(255,255,255,0.5)";
 div.style.borderRadius = "2.5em";
 document.body.appendChild(div);
+
+let t = Date.now();
+const f = () => {
+    if (Date.now() - t >= 5000) {
+        div.style.display = "none";
+    }
+}
+window.setInterval(f,1000);
 
 function reqListener () {
     console.log(this.responseText);
@@ -38,6 +46,8 @@ function reqListener () {
         };
         div.appendChild(span);
     });
+    div.style.display = "flex";
+    t = Date.now();
 }
   
 document.onkeydown = function(event){    
@@ -57,10 +67,11 @@ document.onkeydown = function(event){
     if (event.code === "Enter"){
         var oReq = new XMLHttpRequest();
         oReq.addEventListener("load", reqListener);
-        //oReq.open("POST", "http://localhost:8000");
-        oReq.open("POST", "http://192.168.33.10:8000");
+        oReq.open("POST", "http://localhost:8000");
+        // oReq.open("POST", "http://192.168.33.10:8000");
         oReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         const text = document.querySelector(".js-compose-text").value;
         oReq.send(EncodeHTMLForm({sentence: text.split("\n").slice(-1)[0]}));
     }
 }
+
